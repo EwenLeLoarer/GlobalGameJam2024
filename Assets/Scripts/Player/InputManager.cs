0953403971d6b7cb1ed701bb93a2a17b0d8bb291
Rafale25 <47261384+Rafale25@users.noreply.github.com>
2024-01-27 19:31:10 +0100

    Input manager wip

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInput _playerInput;
+    private InputAction _moveAction;
+
+    void Awake()
     {
-        
+        _playerInput = GetComponent<PlayerInput>();
+        // _moveAction = _playerInput.actions["Move"];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
     }
 }
