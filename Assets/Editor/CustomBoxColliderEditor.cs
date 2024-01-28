41dbca440ba649de496f974cee713068c5779203
Rafale25 <47261384+Rafale25@users.noreply.github.com>
2024-01-28 17:49:32 +0100

    Editor folder + custo; editor test

---
+++ Assets/Editor/CustomBoxColliderEditor.cs
@@ -0,0 +1,21 @@
+// using UnityEditor;
+// using UnityEngine;
+
+// [CustomEditor(typeof (BoxCollider), true)]
+// public class CustomBoxColliderEditor : Editor
+// {
+//     public override void OnInspectorGUI()
+//     {
+//         // draw your stuff
+
+//         // use this to get edit button
+//         var content = EditorGUIUtility.TrTempContent("Edit Collider");
+//         EditorGUILayout.EditorToolbarForTarget(content, (Object) this);
+
+//         // add space for better look
+//         EditorGUILayout.Space(5);
+
+//         // draw everything else
+//         base.OnInspectorGUI();
+//     }
+// }
