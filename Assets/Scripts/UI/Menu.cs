1a4817b629a97bc2babcd701de8bfba2fc133dbd
EwenLeLoarer <ewen.leloarer1@gmail.com>
2024-01-28 21:19:37 +0100

    audio for the menu

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -6,13 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private AudioClip _clip;
     public void Play()
     {
+        AudioManager.PlaySound2D("Click");
         SceneManager.LoadScene("scene Ewen");
     }
 
     public void Exit()
     {
+        AudioManager.PlaySound2D("Click");
 #if UNITY_EDITOR
         EditorApplication.isPlaying = false;
 #else
